641a37cee1afed030c7ca886d7145001a05b3045
VectorWing <lucas.t.barcellos@gmail.com>
2019-07-14 12:39:23 -0300

    Update copa-filmes with FormBuilder code

--- ClientApp/src/app/copa-filmes/copa-filmes.component.ts
+++ ClientApp/src/app/copa-filmes/copa-filmes.component.ts
@@ -1,23 +1,41 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-copa-filmes',
   templateUrl: './copa-filmes.component.html'
 })
 export class CopaFilmesComponent {
-  public filmes: Filme[];
+  
+  filmForm: FormGroup;
+  filmes;
 
-  constructor(http: HttpClient) {
-    http.get<Filme[]>('https://copadosfilmes.azurewebsites.net/api/filmes').subscribe(result => {
-      this.filmes = result;
-    }, error => console.error(error));
+  constructor(http: HttpClient, private fb: FormBuilder) {
+    http.get('https://copadosfilmes.azurewebsites.net/api/filmes').subscribe(
+      result => this.filmes = result,
+      error => console.error(error)
+    );
+
+    this.filmForm = this.fb.group({
+      filmes: new FormArray([])
+    });
+
+    console.log(this.filmes);
+    //this.addCheckboxes();
+  }
+  
+  private addCheckboxes() {
+    this.filmes.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.filmForm.controls.filmes as FormArray).push(control);
+    });
   }
 }
 
-interface Filme {
-  id: string;
-  titulo: string;
-  ano: number;
-  nota: number;
-}
+// interface Filme {
+//   id: string;
+//   titulo: string;
+//   ano: number;
+//   nota: number;
+// }
